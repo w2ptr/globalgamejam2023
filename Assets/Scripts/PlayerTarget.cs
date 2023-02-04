1ed765782d31a79042458ef1db8358f3309870b1
megabro <35005210+megabro@users.noreply.github.com>
2023-02-04 20:14:13 +0100

    Fix joysticks.

--- Assets/Scripts/PlayerTarget.cs
+++ Assets/Scripts/PlayerTarget.cs
@@ -13,9 +13,11 @@ public class PlayerTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float translation1 = Input.GetAxis("Vertical1") * Time.deltaTime * 20;
+        float translation1 = Input.GetAxis("VerticalLeft") * Time.deltaTime * 20;
         transform.Translate(0, 0, translation1);
-        float translation2 = Input.GetAxis("Horizontal1") * Time.deltaTime * 20;
+        float translation2 = Input.GetAxis("HorizontalLeft") * Time.deltaTime * 20;
         transform.Translate(translation2, 0, 0);
+
+        Debug.Log($"Input HorizontalRight: {Input.GetAxis("HorizontalRight")}");
     }
 }
