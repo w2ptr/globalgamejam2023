0d5b9d766911416761e924064bf16146c32df608
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2023-02-04 17:56:31 +0100

    first implementation of BranchRenderer

--- Assets/Scripts/Branch/BranchRenderer.cs
+++ Assets/Scripts/Branch/BranchRenderer.cs
@@ -1,18 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.U2D;
 
-public class BranchRenderer : MonoBehaviour
+namespace GlobalGameJam2023
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Responsible for rendering a branch, points in world space can be set
+    /// 
+    /// Automatically calculates the bezier curve for smooth branch rendering
+    /// </summary>
+    [RequireComponent(typeof(SpriteShapeController))]
+    public class BranchRenderer : MonoBehaviour
     {
-        
+        [SerializeField] private SpriteShapeController _spriteShapeController;
+
+        public float WidthOffset = 0.0f;
+
+        /// <summary>
+        /// Constant for making sure the spline does not stop rendering
+        /// (even if one of the points has a Height of 0.0f, the entire spline
+        /// disappears)
+        /// </summary>
+        private const float k_MinimumSplineWidth = 0.01f;
+
+        [System.Serializable]
+        public struct Point
+        {
+            public float Width;
+            public Vector3 Position; // world space coordinates, perpendicular to object. 
+        }
+
+        [SerializeField] private List<Point> _points;
+
+        private void Awake()
+        {
+            if (_spriteShapeController == null)
+            {
+                _spriteShapeController = GetComponent<SpriteShapeController>();
+            }
+        }
+
+        private void Update()
+        {
+            SetPoints(_points);
+            SetCurve();
+        }
+
+        private void SetCurve()
+        {
+            int splinePointCount = _spriteShapeController.spline.GetPointCount();
+            for (int i = 0; i < splinePointCount; i++)
+            {
+                _spriteShapeController.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
+            }
+        }
+
+        /// <summary>
+        /// List of points in world space
+        /// </summary>
+        private void SetPoints(List<Point> points)
+        {
+            // update the amount of points of the spline
+            int splinePointCount = _spriteShapeController.spline.GetPointCount();
+            
+            // Clear points
+            if (points.Count == 0)
+            {
+                _spriteShapeController.spline.Clear();
+            }
+
+            // Add points
+            if (points.Count > splinePointCount)
+            {
+                //_spriteShapeController.spline.InsertPointAt(splinePointCount, )
+                for (int i=splinePointCount-1; i < points.Count-1; i++)
+                {
+                    Debug.Log($"Added point at {i}");
+                    _spriteShapeController.spline.InsertPointAt(i, points[i].Position);
+                }
+            }
+
+            // Remove points
+            if (points.Count < splinePointCount)
+            {
+                for (int i=splinePointCount-1; i > points.Count-1; i--)
+                {
+                    Debug.Log($"Removed point at {i}");
+                    _spriteShapeController.spline.RemovePointAt(i);
+                }
+            }
+
+            // Set positions
+            int count = points.Count;
+            for (int i=0; i<count; i++)
+            {
+                _spriteShapeController.spline.SetPosition(i, points[i].Position);
+                float width = WidthOffset + points[i].Width;
+                width = Mathf.Clamp(width, k_MinimumSplineWidth, Mathf.Infinity);
+                _spriteShapeController.spline.SetHeight(i, width);
+            }
+
+            Debug.Log(_spriteShapeController.spline.GetPointCount());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
