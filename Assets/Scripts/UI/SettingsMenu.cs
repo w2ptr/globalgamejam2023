714ed93a08d909e331af7bf37e81f963692d6ad9
w2ptr <43069332+w2ptr@users.noreply.github.com>
2023-02-05 10:13:35 +0100

    Add map selection to menu

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+// using GlobalGameJam2023.Globals;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -9,30 +10,39 @@ public class SettingsMenu : MonoBehaviour
     public GameObject overlayA;
     public GameObject optionB;
     public GameObject overlayB;
+    public GameObject optionC;
+    public GameObject overlayC;
+    public GameObject optionD;
+    public GameObject overlayD;
 
     private List<GameObject> options = new List<GameObject>();
     private List<GameObject> overlays = new List<GameObject>();
-
-    // workaround for C# weirdness related to () => stuff
-    private class Integer
-    {
-        public Integer(int x) { this.x = x; }
-        public int x;
-    }
 
     void Start()
     {
         options.Add(optionA);
         options.Add(optionB);
+        options.Add(optionC);
+        options.Add(optionD);
         overlays.Add(overlayA);
         overlays.Add(overlayB);
+        overlays.Add(overlayC);
+        overlays.Add(overlayD);
         
         for (var i = 0; i < options.Count; i++)
         {
-            Integer i2 = new Integer(i);
+            object i2 = i;
             var button = options[i].GetComponent<Button>();
-            button.onClick.AddListener(() => SelectMap(i2.x));
+            button.onClick.AddListener(() => SelectMap((int) i2));
+
+            if (i != 0)
+            {
+                overlays[i].SetActive(false);
+            }
         }
+
+        overlays[0].SetActive(true);
+        PlayerPrefs.SetInt("mapSelection", 0);
     }
 
     private void SelectMap(int x)
@@ -45,5 +55,6 @@ public class SettingsMenu : MonoBehaviour
             }
         }
         overlays[x].SetActive(true);
+        PlayerPrefs.SetInt("mapSelection", x);
     }
 }
